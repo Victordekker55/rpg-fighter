b6b4c5bb979df26070db5fe9062e7fd8079885d4
Victordekker55 <victordekker55@gmail.com>
2022-02-16 13:59:06 +0100

    added attack

--- resources.py
+++ resources.py
@@ -1,15 +1,46 @@
-from pygame import init
-
-
 class Character:
-    def __init__(self, name, health, attack, armour):
+    
+    def __init__(self, name, health, attack, armor):
         self.name = name
         self.health = health
         self.attack = attack
-        self.armour = armour
-
-    
-    
-    def __str__(self) -> str:
-        return_string = f"Name: {self.name}\nHealth: {self.health}\nAttack: {self.attack}\nArmour: {self.armour}"
+        self.armor = armor
         
+    def __str__(self) -> str:
+        return f"Name: {self.name}\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor}"
+    
+    def damage(self):
+        return self.attack
+    
+    def take_damage(self, damage):
+        relative_damage = damage - self.armor
+        if relative_damage > 0:
+            self.health -= relative_damage
+        if self.health < 0: self.health = 0
+
+    def get_health(self):
+        return self.health
+
+class Goblin:
+    
+    def __init__(self, health, attack, armor):
+        self.health = health
+        self.attack = attack
+        self.armor = armor
+        
+    def __str__(self) -> str:
+        return f"Goblin\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor}"
+
+    def damage(self):
+        return self.attack
+    
+    def take_damage(self, damage):
+        relative_damage = damage - self.armor
+        if relative_damage > 0:
+            self.health -= relative_damage
+        if self.health < 0: self.health = 0
+
+    def get_health(self):
+        return self.health
+    
+    
